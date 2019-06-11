1595dfe7949d71f7b5dd23a04a1196d5d9c32f46
Karen McGee <karen.eliz.mcgee@gmail.com>
2019-06-11 18:28:03 -0500

    These are the changes.

---
+++ myfirstpython.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("Hello", "World")
+# this is a comment
+
+print("I am ", 21)
+
+print('I am not really', 21)
+
+print('I am not really 21')
+
+age = 21
+print('I am not really ' + str(age))
+
+print(f'I am really {age-3}')
+print(f'I am really {age}')
+
